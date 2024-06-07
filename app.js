552fb38ded4da2fb5b6f7e10dcf58cdf1eb7a092
Muhammad Usman Saleem <muhammad.usman.saleem@sap.com>
2024-06-07 22:35:13 +0200

    Register button added to index.html

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const http = require('http');
 const path = require('path'); // Import the 'path' module
 const app = express();
 // Set up middleware to serve static files from the current directory
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'img')));
 
 const server = http.createServer(app);
 const PORT = 3000;
