6293451bd826d0a8cc3a3468c2844534a4a70628
Nashitah Zainab <nashitahzainab@Nashitahs-MacBook-Air.local>
2024-06-10 22:37:27 +0200

    Create new function post

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 const fs = require('fs');
 
+
 const express = require('express');
 const http = require('http');
 const path = require('path'); // Import the 'path' module
@@ -11,7 +12,7 @@ console.log(__dirname);
 app.use(express.static(path.join(__dirname, 'html')));
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'img')));
-
+app.use(express.json());
 const server = http.createServer(app);
 const PORT = 3000;
 // Add GET REST endpoint /users/:userid
@@ -20,7 +21,9 @@ app.get('/users/:userid', (req, res) => {
     res.json({ message: `User ID is ${userId}` });
 });
 app.get('/users', (req, res) => {
+    console.log(__dirname);
     const filePath = path.join(__dirname, 'json/users.json');
+    console.log(filePath)
     const data = fs.readFileSync(filePath, 'utf8');
     res.json(JSON.parse(data));
 });
@@ -43,6 +46,25 @@ app.delete('/users/:userid', (req, res) => {
     } else {
         res.status(404).json({ message: 'User not found' });
     }
+}); 
+app.post('/users', (req, res) => {
+    // Read existing users from the file
+    const filePath = path.join(__dirname, 'json/users.json');
+    const usersData = fs.readFileSync(filePath, 'utf8');
+    const users = JSON.parse(usersData);
+
+    // Get new user data from request body
+    const newUser = req.body;
+    console.log(newUser)
+
+    // Add new user to the users array
+    users.push(newUser);
+
+    // Write updated users array back to the file
+    fs.writeFileSync(filePath, JSON.stringify(users, null, 2), 'utf8');
+
+    // Respond with success message
+    res.status(201).json({ message: 'User added successfully!' });
 });
 
 
